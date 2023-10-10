f15445b964e16601ae522509ae80048f77a56ba4
sissis02 <sissis1993@gmail.com>
2023-10-10 14:00:07 +0200

    pass a props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,16 @@
-const pokemonList = [
-    {
-        name: "bulbasaur", 
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    }, 
-    {
-        name: "mew",
-    }
-]
 
-function PokemonCard () {
-    const pokemon = pokemonList[1];
-    let image = pokemon.imgSrc;
 
+function PokemonCard (props) {
+    console.log(props);
+    
+    const { pokemon, image } = props;
+
+    // const pokemon = pokemonList[1];
+    // let image = pokemon.imgSrc;
+    
     return (
         <figure>
-            {typeof image === "string" ? <img src={image} alt="Bulbasaur"/> : <p>???</p>}
+            {typeof pokemon.imgSrc === "string" ? <img src={pokemon.imgSrc} alt="Bulbasaur"/> : <p>???</p>}
         <figcaption>{pokemon.name}</figcaption>
         </figure>
         
