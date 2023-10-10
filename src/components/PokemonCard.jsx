70d20431d2e1f6cd34822f7dbe45260d9183d648
sissis02 <sissis1993@gmail.com>
2023-10-10 17:05:41 +0200

    type props with prop-types

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types';
 
 function PokemonCard (props) {
     console.log(props);
@@ -8,6 +8,13 @@ function PokemonCard (props) {
     // const pokemon = pokemonList[1];
     // let image = pokemon.imgSrc;
     
+    PokemonCard.propTypes = {
+        pokemon:PropTypes.shape({
+            imgSrc: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+        }).isRequired,
+    }
+
     return (
         <figure>
             {typeof pokemon.imgSrc === "string" ? <img src={pokemon.imgSrc} alt="Bulbasaur"/> : <p>???</p>}
