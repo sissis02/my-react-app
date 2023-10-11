e29a6d13dfbbfdeb2abe59bc27d6180cf38c1938
sissis02 <sissis1993@gmail.com>
2023-10-11 14:01:18 +0200

    use the map method

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,32 @@
+import PokemonCard from "./PokemonCard";
+
 function NavBar( {pokemonIndex, pokemonList, setPokemonIndex}) {
 
     const handleClick = () => {
         setPokemonIndex(pokemonIndex + 1);
       }
     
-      const handleClick2 = ()=>  {
+      const handleClick2 = () =>  {
         setPokemonIndex(pokemonIndex - 1);
+      }
+
+      const removeArticle = () => {
+        setPokemonIndex(pokemonIndex.imgSrc);
       }
 
     return (
        <div>
-            {pokemonIndex > 0 ? <button onClick={handleClick2}>Précédent</button> : ""}
-            {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClick}>Suivant</button> : ""}  
+          {pokemonList.map((pokemonIndex, index) => (
+            <button key={index} onClick={() => removeArticle(pokemonIndex.imgSrc)}>{pokemonIndex.name}</button>
+          ) )}  
         </div> 
     );
 }
 
 export default NavBar; 
 
+
+/*
+{pokemonIndex > 0 ? <button onClick={handleClick2}>Précédent</button> : ""}
+            {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClick}>Suivant</button> : ""} 
+            */
